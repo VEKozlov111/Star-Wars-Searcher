0f671cc5bd62c680ca1957b2396be3e3551d81de
Vladislav Kozlov <vlad.kozlov.2015@gmail.com>
2024-02-13 20:29:20 +0300

    feat: add Search.tsx

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import data from "../assets/data.json";
 import Card from "../components/Card";
+import Search from "../components/Search";
 
 function Main() {
   return (
     <div >
-      <div className="flex  gap-4 pt-2 flex-wrap">
+      <Search />
+      <div className="flex  gap-[1.5vw] pt-2 flex-wrap justify-center">
         {data.products.map((card) => (
           <Card card={card} key={card.id} />
         ))}
