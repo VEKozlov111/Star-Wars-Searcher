0e45d95bcf28ab744e492e7407d5bc3663cc9bb5
Vladislav Kozlov <vlad.kozlov.2015@gmail.com>
2024-02-13 19:52:14 +0300

    fix: fix NotFoundPage

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function NotFoundPage() {
   return (
     <div className="h-full flex flex-col gap-4 items-center justify-center">
-      <p className="text-color1 text-xl">This page does not exist!</p>
+      <p className="text-color3 text-xl">This page does not exist!</p>
       <Link to="/" className="btn">
         Main page
       </Link>
