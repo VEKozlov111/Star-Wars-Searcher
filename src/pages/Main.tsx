16a137e45006b4366d7c93f8929b8f2776e14e4d
Vladislav Kozlov <vlad.kozlov.2015@gmail.com>
2024-02-13 20:13:43 +0300

    feat: add card

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import data from "../assets/data.json";
+import Card from "../components/Card";
 
 function Main() {
   return (
     <div >
-      Main
+      <div className="flex  gap-4 pt-2 flex-wrap">
+        {data.products.map((card) => (
+          <Card card={card} key={card.id} />
+        ))}
+      </div>
     </div>
   );
 }
